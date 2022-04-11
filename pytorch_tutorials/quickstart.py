4f3339a3abe1c64cd6f9ad8c13baff0610f0d0db
ss-hyun <yunatlsghk6@ajou.ac.kr>
2022-04-11 20:10:29 +0900

    practice quick start

--- pytorch_tutorials/quickstart.py
+++ pytorch_tutorials/quickstart.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+# Download training data from open datasets.
 training_data = datasets.FashionMNIST(
     root="data",
     train=True,
@@ -11,7 +12,9 @@ training_data = datasets.FashionMNIST(
     transform=ToTensor(),
 )
 
-test_data = datasets. FashionMNIST(
+# Download test data from open datasets.
+
+test_data = datasets.FashionMNIST(
     root="data",
     train=False,
     download=True,
@@ -24,8 +27,35 @@ batch_size = 64
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
-for X, y in test_dataloader:
-    print(f"Shape of X [N, C, H, W]: {X.shape}")
-    print(f"Shape of y: {y.shape} {y.dtype}")
-    print(y)
-    break
+# for X, y in test_dataloader:
+#     print(f"Shape of X [N, C, H, W]: {X.shape}")
+#     print(f"Shape of y: {y.shape} {y.dtype}")
+#     print(y)
+#     break
+
+# Get using device type for training
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'Using {device} device')
+
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),  # activation function
+            nn.Linear(512, 512),
+            nn.ReLU(),  # activation function
+            nn.Linear(512, 10)  # predict 10 class
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)  # 1 * 28 * 28 -> 784 ( 3D -> 1D )
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+model = NeuralNetwork().to(device)
+print(model)
