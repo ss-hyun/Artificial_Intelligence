92f37f9e6176af1b901be772fed0e2ef0b95ead5
ss-hyun <yunatlsghk6@ajou.ac.kr>
2022-04-15 23:35:32 +0900

    Practice weight initialization 2

--- weight_init.py
+++ weight_init.py
@@ -1,22 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-dims = [4096] * 7
-hs = []
-x = np.random.randn(16, dims[0])
-for Din, Dout in zip(dims[:-1], dims[1:]):
-    # small random numbers
-    W = 0.01 * np.random.randn(Din, Dout)
-    print(W.shape)
-    x = np.tanh(x.dot(W))
-    print(x.shape)
-    hs.append(x)
 
-fig, ax = plt.subplots()
-print(fig, ax)
-for i in range(0, len(hs)):
-    H, bins = np.histogram(hs[i], bins=20)
-    print(H, bins)
-    ax.plot(bins, np.insert(H, 0, H[0]))
-ax.legend("0123456")
+def small(Din, Dout):
+    return 0.01 * np.random.randn(Din, Dout)
+
+
+def large(Din, Dout):
+    return 0.05 * np.random.randn(Din, Dout)
+
+
+def Xavier(Din, Dout):
+    return np.random.randn(Din, Dout) * np.sqrt(2/Din)
+
+
+def ReLU(x):
+    return np.maximum(0, x)
+
+
+active = {
+    small: np.tanh,
+    large: np.tanh,
+    Xavier: ReLU
+}
+
+fig = plt.figure(figsize=(13, 9))
+size = (2, 2)
+
+for idx, label in enumerate([small, large, Xavier]):
+    dims = [4096] * 7
+    hs = []
+    x = np.random.randn(16, dims[0])
+    for Din, Dout in zip(dims[:-1], dims[1:]):
+        W = label(Din, Dout)
+        x = active[label](x.dot(W))
+        hs.append(x)
+
+    ax = fig.add_subplot(size[0], size[1], idx + 1)
+    for i in range(0, len(hs)):
+        H, bins = np.histogram(hs[i], bins=20)
+        # print(H, bins)
+        ax.plot(bins, np.insert(H, 0, H[0]))
+    ax.legend("0123456")
+
+fig.tight_layout()
 plt.show()
